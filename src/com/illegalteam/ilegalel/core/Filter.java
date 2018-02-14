2a1ebccb8b8a7a0da1aedae1c174ab0bf4ecbdc6
maxim <godofmadness>
2018-02-14 14:50:12 +0300

    initial commit

---
+++ src/com/illegalteam/ilegalel/core/Filter.java
@@ -0,0 +1,24 @@
+package com.illegalteam.ilegalel.core;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by mm on 2/14/18.
+ */
+public class Filter implements javax.servlet.Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+
+
+    public void destroy() {
+
+    }
+}
+s
---
+++ src/com/illegalteam/ilegalel/core/Servlet.java
@@ -0,0 +1,21 @@
+package com.illegalteam.ilegalel.core;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mm on 2/14/18.
+ */
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("get");
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
